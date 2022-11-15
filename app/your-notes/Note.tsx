4e56e535b7578f2eaa22f2a99a0c22cbacdca808
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-15 18:31:18 +0100

    adjust note UI

--- app/your-notes/Note.tsx
+++ app/your-notes/Note.tsx
@@ -7,7 +7,7 @@ import { useMutation, useQuery } from '@apollo/client';
 import { useRouter } from 'next/navigation';
 
 export default function Note() {
-  const { data, loading, error } = useQuery(AllNotesQuery);
+  const { data } = useQuery(AllNotesQuery);
   const [resultDeleteNote] = useMutation(DELETE_NOTE, {
     refetchQueries: [{ query: AllNotesQuery }, 'AllNotesQuery'],
   });
@@ -25,18 +25,22 @@ export default function Note() {
     <div>
       {data?.notes?.map(({ title, content, id }: Notes) => (
         <div key={id as string}>
-          <h1 className="text-s font-medium text-gray-500">Title: </h1>
-          <h2 className="text-s font-medium text-white">{title}</h2>
-          <h1 className="text-s font-medium text-gray-500">Content: </h1>
-          <h5 className="text-s font-medium text-white">{content}</h5>
-          <br />
-          <div>
-            <button
-              className="rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-100"
+          <div className="max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {title}
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              {content}
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={() => deleteNote(id)}
             >
               Delete Note
-            </button>
+            </a>
           </div>
         </div>
       ))}
--- app/your-notes/loading.tsx
+++ app/your-notes/loading.tsx
@@ -5,6 +5,7 @@ export default function Loading() {
     <div>
       {' '}
       <SkeletonCard />
+      <SkeletonCard />
     </div>
   );
 }
