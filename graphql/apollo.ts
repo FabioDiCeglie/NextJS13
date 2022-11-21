a18d77f9029a8d3a532237e1849280679adbaf93
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-21 20:50:18 +0100

    quix fix

--- graphql/apollo.ts
+++ graphql/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: 'https://next-js-13.vercel.app/api/graphql',
+  uri: 'https://next-js-13.vercel.app/api/auth',
   cache: new InMemoryCache(),
 });
 
