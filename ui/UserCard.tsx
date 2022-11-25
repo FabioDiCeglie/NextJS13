484191ab771507688aae2e595de988ee1ba57a9f
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-25 17:34:16 +0100

    fixed log out on mobile

--- ui/UserCard.tsx
+++ ui/UserCard.tsx
@@ -7,7 +7,7 @@ export default function UserCard({ session }: any) {
       <button
         onClick={() => signOut()}
         className={clsx(
-          'block rounded-md px-3 py-2 text-sm font-medium text-gray-100 hover:bg-gray-800 hover:text-gray-100 md:hidden lg:hidden',
+          'font-small  ml-3 block  py-4 text-sm text-gray-100 hover:text-gray-100 md:hidden lg:hidden',
         )}
       >
         Log Out
