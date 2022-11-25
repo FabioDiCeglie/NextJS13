553dd3a182636de2f5e2b45c8edbe54e8efdf909
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-25 17:36:28 +0100

    fix some styling for mobile

--- ui/EmptyDashboard.tsx
+++ ui/EmptyDashboard.tsx
@@ -1,8 +1,8 @@
 export const EmptyDashboard = () => (
   <div>
-    <div className="mb-10 max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800">
+    <div className="mb-10 w-40  max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800 md:w-full lg:w-full">
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        Any notes available
+        Any available
       </p>
     </div>
   </div>
